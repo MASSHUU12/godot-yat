e7b543af6827e9e862209853f4ffd89f332dff50
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-10-26 18:50:09 +0200

    Remove variables from IYatCommand interface

--- addons/yat/YatCommand.cs
+++ addons/yat/YatCommand.cs
@@ -1,22 +1,6 @@
 public partial interface IYatCommand
 {
 	/// <summary>
-	/// Gets the name of the CLI.
-	/// </summary>
-	public string Name { get; }
-	/// <summary>
-	/// Gets the description of the CLI command.
-	/// </summary>
-	public string Description { get; }
-	/// <summary>
-	/// Gets the usage information for the command line interface.
-	/// </summary>
-	public string Usage { get; }
-	/// <summary>
-	/// Gets the aliases for this command.
-	/// </summary>
-	public string[] Aliases { get; }
-	/// <summary>
 	/// Executes the YAT command with the given arguments.
 	/// </summary>
 	/// <param name="yat">The YAT instance to execute the command on.</param>
