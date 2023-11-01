06368a071a6c210324060153ce0d7d40ee2506da
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-01 16:32:52 +0100

    Extract print logic to the separate method

--- addons/yat/commands/builtin/Man.cs
+++ addons/yat/commands/builtin/Man.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Text;
+using YAT.Helpers;
 
 namespace YAT.Commands
 {
@@ -17,33 +17,49 @@ namespace YAT.Commands
 			var lookup = yat.Commands;
 			var commandName = args[1];
 
-			if (lookup.ContainsKey(commandName))
-			{
-				var command = lookup[commandName];
-				CommandAttribute attribute = Attribute.GetCustomAttribute(command.GetType(), typeof(CommandAttribute)) as CommandAttribute;
-				StringBuilder sb = new();
-
-				sb.AppendLine($"[p align=center][font_size=22]{attribute.Name}[/font_size][/p]");
-				sb.AppendLine($"[p align=center]{attribute.Description}[/p]");
-				sb.AppendLine(attribute.Manual);
-				sb.AppendLine("\n[b]Aliases[/b]:");
-
-				if (attribute.Aliases.Length > 0)
-				{
-					foreach (var alias in attribute.Aliases)
-						sb.Append($"[ul]\t{alias}[/ul]");
-				}
-				else sb.AppendLine("[ul]\tNone[/ul]");
-
-				yat.Terminal.Println(sb.ToString());
-			}
-			else
+			if (!lookup.ContainsKey(commandName))
 			{
 				yat.Terminal.Println($"Unknown command: {commandName}");
 				return CommandResult.InvalidCommand;
 			}
 
+			var command = lookup[commandName];
+
+			CommandAttribute attribute = AttributeHelper.GetAttribute<CommandAttribute>(command);
+
+			if (attribute is null || string.IsNullOrEmpty(attribute.Manual))
+			{
+				yat.Terminal.Println($"Command {commandName} does not have a manual.");
+				return CommandResult.Failure;
+			}
+
+			PrintManual(yat, attribute);
+
 			return CommandResult.Success;
+		}
+
+		/// <summary>
+		/// Prints the manual for a given command.
+		/// </summary>
+		/// <param name="yat">The YAT instance.</param>
+		/// <param name="attribute">The CommandAttribute of the command to print the manual for.</param>
+		private static void PrintManual(YAT yat, CommandAttribute attribute)
+		{
+			StringBuilder sb = new();
+
+			sb.AppendLine($"[p align=center][font_size=22]{attribute.Name}[/font_size][/p]");
+			sb.AppendLine($"[p align=center]{attribute.Description}[/p]");
+			sb.AppendLine(attribute.Manual);
+			sb.AppendLine("\n[b]Aliases[/b]:");
+
+			if (attribute.Aliases.Length > 0)
+			{
+				foreach (var alias in attribute.Aliases)
+					sb.Append($"[ul]\t{alias}[/ul]");
+			}
+			else sb.AppendLine("[ul]\tNone[/ul]");
+
+			yat.Terminal.Println(sb.ToString());
 		}
 	}
 }
