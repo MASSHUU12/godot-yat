faec92b16caee023a1e60d270870d318e3132eb4
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-01 16:53:13 +0100

    Add manual field for extensions

--- addons/yat/commands/Extension.cs
+++ addons/yat/commands/Extension.cs
@@ -10,12 +10,14 @@ namespace YAT.Commands
 	{
 		public string Name { get; private set; }
 		public string Description { get; private set; }
+		public string Manual { get; private set; }
 		public string[] Aliases { get; private set; }
 
-		public ExtensionAttribute(string name, string description = "", params string[] aliases)
+		public ExtensionAttribute(string name, string description = "", string manual = "", params string[] aliases)
 		{
 			Name = name;
 			Description = description;
+			Manual = manual;
 			Aliases = aliases;
 		}
 	}
