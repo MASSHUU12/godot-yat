3c43cc8c3e5597afedb68b9c77330494739c1923
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-01 15:41:14 +0100

    Create basic set command

---
+++ addons/yat/commands/builtin/Set.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+namespace YAT.Commands
+{
+	[Command("set", "Sets a variable to a value.", "[b]Usage[/b]: set [i]variable[/i] [i]value[/i]")]
+	public partial class Set : ICommand, IExtensible
+	{
+		public Extensible Extension => new();
+
+		public CommandResult Execute(YAT yat, params string[] args)
+		{
+			if (args.Length < 3)
+			{
+				yat.Terminal.Println("Invalid arguments.", Terminal.PrintType.Error);
+				return CommandResult.InvalidArguments;
+			}
+
+			var variable = args[1];
+
+			GD.Print(variable);
+			GD.Print(Extension.Extensions);
+
+			// if (Extension.Extensions.ContainsKey(variable))
+			// {
+			// 	var extension = Extension.Extensions[variable];
+			// 	return extension.Execute(yat, this, args[2..]);
+			// }
+
+			yat.Terminal.Println("Variable not found.", Terminal.PrintType.Error);
+			return CommandResult.Failure;
+		}
+	}
+}
