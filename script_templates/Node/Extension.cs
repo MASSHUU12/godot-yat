00fb450e610f70f9ae71ca82078d048512be056d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-01 17:12:35 +0100

    Create command extension template

---
+++ script_templates/Node/Extension.cs
@@ -0,0 +1,15 @@
+// meta-description: Base template for YAT commands extension.
+
+using _BINDINGS_NAMESPACE_;
+using YAT;
+
+[Extension("_CLASS_", "Lorem ipsum dolor sit amet.", "[b]Usage[/b]: _CLASS_")]
+public partial class _CLASS_ : IExtension
+{
+	public CommandResult Execute(YAT.YAT yat, ICommand command, params string[] args)
+	{
+		yat.Terminal.Print("_CLASS_ is not yet implemented!", Terminal.PrintType.Warning);
+
+		return CommandResult.Failure;
+	}
+}
