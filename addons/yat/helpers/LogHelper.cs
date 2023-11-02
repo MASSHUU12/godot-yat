6da91e6a30cbf3f41be615381bf78ded577b74f6
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-02 13:51:08 +0100

    Add PrintMessage method to LogHelper.cs

--- addons/yat/helpers/LogHelper.cs
+++ addons/yat/helpers/LogHelper.cs
@@ -18,13 +18,25 @@ namespace YAT.Helpers
 		private static bool _terminalValid = false;
 
 		/// <summary>
+		/// Prints a message to the console or Godot terminal.
+		/// </summary>
+		/// <param name="message">The message to print.</param>
+		/// <param name="printType">The type of message to print.</param>
+		/// <param name="useGDTerminal">Whether to print also to the Godot terminal.</param>
+		private static void PrintMessage(string message, Terminal.PrintType printType, bool useGDTerminal = false)
+		{
+			if (useGDTerminal) GD.PushError(message);
+			if (_terminalValid) Terminal.Println(message, printType);
+		}
+
+		/// <summary>
 		/// Prints an error message indicating that an unknown command was entered.
 		/// </summary>
 		/// <param name="command">The unknown command that was entered.</param>
 		public static void UnknownCommand(string command)
 		{
-			if (!_terminalValid) return;
-			Terminal.Println($"Unknown command: {command}", Terminal.PrintType.Error);
+			var message = $"Unknown command: {command}";
+			PrintMessage(message, Terminal.PrintType.Error);
 		}
 	}
 }
