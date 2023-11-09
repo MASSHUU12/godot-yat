f63ef37321cc9592983e8ac4f3364e9b394e4bdb
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-09 11:13:00 +0100

    Refactor Cls command to use Yat property instead of parameter.

--- addons/yat/src/commands/builtin/Cls.cs
+++ addons/yat/src/commands/builtin/Cls.cs
@@ -1,19 +1,22 @@
 namespace YAT.Commands
 {
 	[Command(
-	"cls",
-	"Clears the console.",
-	"[b]Usage[/b]: cls",
-	"clear"
-)]
+		"cls",
+		"Clears the console.",
+		"[b]Usage[/b]: cls",
+		"clear"
+	)]
 	public partial class Cls : ICommand
 	{
-		public CommandResult Execute(YAT yat, params string[] args)
+		public YAT Yat { get; set; }
+
+		public Cls(YAT Yat) => this.Yat = Yat;
+
+		public CommandResult Execute(params string[] args)
 		{
-			yat.Terminal.Clear();
+			Yat.Terminal.Clear();
 
 			return CommandResult.Success;
 		}
 	}
-
 }
