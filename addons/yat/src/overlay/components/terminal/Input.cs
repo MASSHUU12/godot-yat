a72c9a103bf365d2fe823e1249bafd80f3e4c79b
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-11 22:53:45 +0100

    Add Input.cs and reference it in Terminal.tscn

---
+++ addons/yat/src/overlay/components/terminal/Input.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class Input : LineEdit
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
