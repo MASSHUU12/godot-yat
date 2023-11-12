c81173fd3bd1a5849366c48a964940466c25c1da
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-12 20:30:08 +0100

    Update templates

--- script_templates/Node/Extension.cs
+++ script_templates/Node/Extension.cs
@@ -1,7 +1,9 @@
 // meta-description: Base template for YAT commands extension.
 
 using _BINDINGS_NAMESPACE_;
-using YAT;
+using YAT.Attributes;
+using YAT.Enums;
+using YAT.Interfaces;
 
 [Extension("_CLASS_", "Lorem ipsum dolor sit amet.", "[b]Usage[/b]: _CLASS_")]
 public partial class _CLASS_ : IExtension
