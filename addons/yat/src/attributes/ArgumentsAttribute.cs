23be91e3cbbe3fa5f9c919c69cfc1b81ecc25ab6
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-13 15:39:49 +0100

    Add comment about args definition

--- addons/yat/src/attributes/ArgumentsAttribute.cs
+++ addons/yat/src/attributes/ArgumentsAttribute.cs
@@ -3,9 +3,10 @@ namespace YAT.Attributes
 	[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false)]
 	public sealed class ArgumentsAttribute : System.Attribute
 	{
-		// TODO: Add optional type validation.
-		// For example: "min:int"
-
+		// Arguments are defined as follows:
+		// arg_name - passed argument must match the name of the argument
+		// arg_name:data_type - passed argument must be of the specified data type
+		// arg_name:[option1, option2, option3] - passed argument must be one of the specified options
 		public string[] Args { get; private set; }
 
 		public ArgumentsAttribute(params string[] args)
