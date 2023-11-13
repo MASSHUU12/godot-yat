d0d665ef7c6bf6a8c37461e9b9b8fc4c77562378
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-13 20:22:38 +0100

    Add TryConvert method to NumericHelper

--- addons/yat/src/helpers/NumericHelper.cs
+++ addons/yat/src/helpers/NumericHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 
 namespace YAT.Helpers
 {
@@ -15,6 +16,35 @@ namespace YAT.Helpers
 		public static bool IsWithinRange<T>(T value, T min, T max) where T : IComparable<T>
 		{
 			return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+		}
+
+		/// <summary>
+		/// Tries to convert the specified string representation of a number to its type T equivalent.
+		/// </summary>
+		/// <typeparam name="T">The type of the output.</typeparam>
+		/// <param name="input">The string to convert.</param>
+		/// <param name="output">When this method returns, contains the converted value if the conversion succeeded, or the default value of T if the conversion failed.</param>
+		/// <returns>true if the conversion succeeded; otherwise, false.</returns>
+		public static bool TryConvert<T>(this string input, out T output) where T : IConvertible, IComparable
+		{
+			var converter = TypeDescriptor.GetConverter(typeof(T));
+
+			if (converter.CanConvertFrom(typeof(string)))
+			{
+				try
+				{
+					output = (T)converter.ConvertFrom(input);
+					return true;
+				}
+				catch (Exception)
+				{
+					output = default;
+					return false;
+				}
+			}
+
+			output = default;
+			return false;
 		}
 	}
 }
