e96d07e16a9a3a5e01ce9399b6c99984cf07d25c
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-14 12:07:00 +0100

    Update plan for options attribute

--- addons/yat/src/attributes/OptionsAttribute.cs
+++ addons/yat/src/attributes/OptionsAttribute.cs
@@ -1,15 +1,28 @@
+using System.Collections.Generic;
+
 namespace YAT.Attributes
 {
 	[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false)]
 	public sealed class OptionsAttribute : System.Attribute
 	{
-		// TODO: Add support for optional arguments after =.
-
-		public string[] Options { get; private set; }
+		// TODO
+		// Options are defined as follows and are optional:
+		// -arg or --arg - passed argument must match the name of the argument
+		// -arg:data_type - passed argument must be of the specified data type
+		//					and look like this: -arg=value
+		// -arg:[option1, data_type, option3] - passed argument must be
+		//										of the specified data types or
+		//										one of the specified options
+		//										many arguments can be passed
+		//										like this: -arg=value1,value2
+		// Numeric data types can have a range specified as follows:
+		// data_type(min, max)
+		public Dictionary<string, object> Options { get; private set; }
 
 		public OptionsAttribute(params string[] options)
 		{
-			Options = options;
+			// TODO: Parse options
+			// Options = options;
 		}
 	}
 }
