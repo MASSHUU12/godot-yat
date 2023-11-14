5a458b2ecde8db67cf1fe1946d2543dbd4f644a9
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-14 15:55:08 +0100

    Parse options passed to options attribute

--- addons/yat/src/attributes/OptionsAttribute.cs
+++ addons/yat/src/attributes/OptionsAttribute.cs
@@ -5,12 +5,11 @@ namespace YAT.Attributes
 	[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false)]
 	public sealed class OptionsAttribute : System.Attribute
 	{
-		// TODO
 		// Options are defined as follows and are optional:
 		// -arg or --arg - passed argument must match the name of the argument
-		// -arg:data_type - passed argument must be of the specified data type
+		// -arg=data_type - passed argument must be of the specified data type
 		//					and look like this: -arg=value
-		// -arg:[option1, data_type, option3] - passed argument must be
+		// -arg=option1,data_type,option3 - passed argument must be
 		//										of the specified data types or
 		//										one of the specified options
 		//										many arguments can be passed
@@ -21,8 +20,47 @@ namespace YAT.Attributes
 
 		public OptionsAttribute(params string[] options)
 		{
-			// TODO: Parse options
-			// Options = options;
+			Options = GetArguments(options);
+		}
+
+		/// <summary>
+		/// Parses the given command line arguments and returns a dictionary
+		/// of argument names and their corresponding values.
+		/// </summary>
+		/// <param name="args">The command line arguments to parse.</param>
+		/// <returns>A dictionary of argument names and their corresponding values.</returns>
+		private static Dictionary<string, object> GetArguments(string[] args)
+		{
+			Dictionary<string, object> arguments = new();
+
+			if (args is null) return arguments;
+
+			foreach (string arg in args)
+			{
+				string[] parts = arg.Split('=');
+				string argName = parts[0];
+				string dataType = parts.Length > 1 ? parts[1] : null;
+
+				arguments.Add(argName, ParseDataType(dataType));
+			}
+
+			return arguments;
+		}
+
+		/// <summary>
+		/// Parses the given data type string and returns an object representing the parsed data.
+		/// </summary>
+		/// <param name="dataType">The data type string to parse.</param>
+		/// <returns>An object representing the parsed data.</returns>
+		private static object ParseDataType(string dataType)
+		{
+			var data = dataType?.Trim();
+
+			if (string.IsNullOrEmpty(data)) return null;
+
+			var splitted = data.Split(',');
+
+			return splitted.Length > 1 ? splitted : splitted[0];
 		}
 	}
 }
