38d83ccd44664cbe15a844eb7f024000020d85a7
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-17 11:56:57 +0100

    Add main menu with start and quit buttons

---
+++ example/main_menu/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _ready() -> void:
+	get_node("%Start").connect("pressed", _on_start_pressed)
+	get_node("%Quit").connect("pressed", _on_quit_pressed)
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://example/game/Game.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
