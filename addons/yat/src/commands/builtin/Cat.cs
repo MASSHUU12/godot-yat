527b565ac6b410006710d5a8353a4cbc55b82435
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-24 10:57:27 +0100

    Working cat command

--- addons/yat/src/commands/builtin/Cat.cs
+++ addons/yat/src/commands/builtin/Cat.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using Godot;
 using YAT.Attributes;
 using YAT.Enums;
 using YAT.Interfaces;
+using YAT.Scenes.Overlay.Components.Terminal;
 
 namespace YAT.Commands
 {
@@ -16,11 +18,35 @@ namespace YAT.Commands
 
 		public CommandResult Execute(Dictionary<string, object> cArgs, params string[] args)
 		{
-			int limit = -1;
-			if (cArgs.TryGetValue("-l", out object l) && l is not null)
-				limit = (int)l;
+			string fileName = (string)cArgs["file"];
+			int lineLimit = -1;
 
-			Yat.Terminal.Print($"{limit}");
+			if (cArgs.TryGetValue("-l", out object l) && l is not null)
+				lineLimit = (int)l;
+
+			if (!FileAccess.FileExists(fileName))
+			{
+				Yat.Terminal.Print($"File '{fileName}' does not exist.", Terminal.PrintType.Error);
+				return CommandResult.InvalidArguments;
+			}
+
+			using FileAccess file = FileAccess.Open(fileName, FileAccess.ModeFlags.Read);
+			int lineCount = 0;
+
+			while (!file.EofReached())
+			{
+				string line = file.GetLine();
+				if (lineLimit > 0 && ++lineCount > lineLimit)
+				{
+					Yat.Terminal.Print(
+						$"Line limit of {lineLimit} reached.",
+						Terminal.PrintType.Warning
+					);
+					break;
+				}
+
+				Yat.Terminal.Print(line);
+			}
 
 			return CommandResult.Success;
 		}
