875d37ded271eace5c651990620da6dcd3d82451
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-11-27 22:43:40 +0100

    Implement quickcommands command

--- addons/yat/src/commands/builtin/QuickCommands.cs
+++ addons/yat/src/commands/builtin/QuickCommands.cs
@@ -1,6 +1,8 @@
 using YAT.Attributes;
 using YAT.Enums;
+using YAT.Helpers;
 using YAT.Interfaces;
+using YAT.Scenes.Overlay.Components.Terminal;
 
 namespace YAT.Commands
 {
@@ -14,7 +16,38 @@ namespace YAT.Commands
 
 		public CommandResult Execute(System.Collections.Generic.Dictionary<string, object> cArgs, params string[] args)
 		{
-			return CommandResult.Success;
+			string action = (string)cArgs["action"];
+			string name = (string)cArgs["name"];
+			string command = (string)cArgs["command"];
+
+			bool status = true;
+			string message;
+
+			switch (action)
+			{
+				case "add":
+					status = Yat.Terminal.Context.QuickCommands.AddQuickCommand(name, command);
+					if (status) message = $"[b]Added quick command '{name}'[/b]";
+					else message = $"[b]Failed to add quick command '{name}'[/b]";
+
+					Yat.Terminal.Print(message, status ? Terminal.PrintType.Success : Terminal.PrintType.Error);
+					break;
+				case "remove":
+					status = Yat.Terminal.Context.QuickCommands.RemoveQuickCommand(name);
+					if (status) message = $"[b]Removed quick command '{name}'[/b]";
+					else message = $"[b]Failed to remove quick command '{name}'[/b]";
+
+					Yat.Terminal.Print(message, status ? Terminal.PrintType.Success : Terminal.PrintType.Error);
+					break;
+				default:
+					foreach (var qc in Yat.Terminal.Context.QuickCommands.QuickCommands.Commands)
+					{
+						Yat.Terminal.Print($"[b]{qc.Key}[/b] - {TextHelper.EscapeBBCode(qc.Value)}");
+					}
+					break;
+			}
+
+			return status ? CommandResult.Success : CommandResult.Failure;
 		}
 	}
 }
