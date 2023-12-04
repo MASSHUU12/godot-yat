37035c0c27d67555a5e0e5a4e38133044005f5c8
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-04 13:06:19 +0100

    Update AttributeHelper.cs to use nullable reference types

--- addons/yat/src/helpers/AttributeHelper.cs
+++ addons/yat/src/helpers/AttributeHelper.cs
@@ -13,7 +13,7 @@ namespace YAT.Helpers
 		/// <typeparam name="T">The type of the attribute to get.</typeparam>
 		/// <param name="obj">The object to get the attribute from.</param>
 		/// <returns>The attribute of the specified type, or null if the object does not have the attribute.</returns>
-		public static T GetAttribute<T>(this object obj) where T : Attribute
+		public static T? GetAttribute<T>(this object obj) where T : Attribute
 		{
 			if (Attribute.GetCustomAttribute(obj.GetType(), typeof(T))
 				is not T attribute
@@ -28,7 +28,7 @@ namespace YAT.Helpers
 		/// <typeparam name="T">The type of attribute to retrieve.</typeparam>
 		/// <param name="obj">The object from which to retrieve the attributes.</param>
 		/// <returns>An array of attributes of the specified type, or null if no attributes are found.</returns>
-		public static T[] GetAttributes<T>(this object obj) where T : Attribute
+		public static T[]? GetAttributes<T>(this object obj) where T : Attribute
 		{
 			if (Attribute.GetCustomAttributes(obj.GetType(), typeof(T))
 				is not T[] attributes
