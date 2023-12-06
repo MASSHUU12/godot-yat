e2856d3bc54ab8b5e966592158f4d6a97a7bfc24
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-06 22:44:31 +0100

    Move SettingsWindow under YAT.Scenes.SettingsWindow namespace

--- addons/yat/src/scenes/settings_window/SettingsWindow.cs
+++ addons/yat/src/scenes/settings_window/SettingsWindow.cs
@@ -1,95 +1,98 @@
 using Godot;
-using YAT.Scenes.YatWindow;
 
-public partial class SettingsWindow : YatWindow
+namespace YAT.Scenes.SettingsWindow
 {
-	private YAT.YAT _yat;
-
-	private Button _save;
-	private Button _load;
-	private Button _restoreDefaults;
-
-	private LineEdit _prompt;
-	private CheckBox _movable;
-	private SpinBox _width;
-	private SpinBox _height;
-	private CheckBox _autoScroll;
-	private CheckBox _showPrompt;
-
-	public override void _Ready()
+	public partial class SettingsWindow : YatWindow.YatWindow
 	{
-		_yat = GetNode<YAT.YAT>("/root/YAT");
+		private YAT _yat;
 
-		_save = GetNode<Button>("%Save");
-		_save.Pressed += _yat.OptionsManager.Save;
+		private Button _save;
+		private Button _load;
+		private Button _restoreDefaults;
 
-		_load = GetNode<Button>("%Load");
-		_load.Pressed += _yat.OptionsManager.Load;
+		private LineEdit _prompt;
+		private CheckBox _movable;
+		private SpinBox _width;
+		private SpinBox _height;
+		private CheckBox _autoScroll;
+		private CheckBox _showPrompt;
 
-		_restoreDefaults = GetNode<Button>("%RestoreDefaults");
-		_restoreDefaults.Pressed += _yat.OptionsManager.RestoreDefaults;
+		public override void _Ready()
+		{
+			_yat = GetNode<YAT>("/root/YAT");
 
-		_prompt = GetNode<LineEdit>("%Prompt");
-		_prompt.Text = _yat.Options.Prompt;
+			_save = GetNode<Button>("%Save");
+			_save.Pressed += _yat.OptionsManager.Save;
 
-		_movable = GetNode<CheckBox>("%Movable");
-		_movable.ButtonPressed = _yat.Options.WindowMovable;
+			_load = GetNode<Button>("%Load");
+			_load.Pressed += _yat.OptionsManager.Load;
 
-		_width = GetNode<SpinBox>("%Width");
-		_width.Value = _yat.Options.DefaultWidth;
+			_restoreDefaults = GetNode<Button>("%RestoreDefaults");
+			_restoreDefaults.Pressed += _yat.OptionsManager.RestoreDefaults;
 
-		_height = GetNode<SpinBox>("%Height");
-		_height.Value = _yat.Options.DefaultHeight;
+			_prompt = GetNode<LineEdit>("%Prompt");
+			_prompt.Text = _yat.Options.Prompt;
 
-		_autoScroll = GetNode<CheckBox>("%AutoScroll");
-		_autoScroll.ButtonPressed = _yat.Options.AutoScroll;
+			_movable = GetNode<CheckBox>("%Movable");
+			_movable.ButtonPressed = _yat.Options.WindowMovable;
 
-		_showPrompt = GetNode<CheckBox>("%ShowPrompt");
-		_showPrompt.ButtonPressed = _yat.Options.ShowPrompt;
+			_width = GetNode<SpinBox>("%Width");
+			_width.Value = _yat.Options.DefaultWidth;
 
-		ConnectSignals();
+			_height = GetNode<SpinBox>("%Height");
+			_height.Value = _yat.Options.DefaultHeight;
+
+			_autoScroll = GetNode<CheckBox>("%AutoScroll");
+			_autoScroll.ButtonPressed = _yat.Options.AutoScroll;
+
+			_showPrompt = GetNode<CheckBox>("%ShowPrompt");
+			_showPrompt.ButtonPressed = _yat.Options.ShowPrompt;
+
+			ConnectSignals();
+		}
+
+		private void ConnectSignals()
+		{
+			CloseRequested += () => QueueFree();
+
+			_prompt.TextSubmitted += (string text) =>
+			{
+				_yat.Options.Prompt = text;
+				UpdateOptions();
+			};
+
+			_movable.Toggled += (bool pressed) =>
+			{
+				_yat.Options.WindowMovable = pressed;
+				UpdateOptions();
+			};
+
+			_width.ValueChanged += (double value) =>
+			{
+				_yat.Options.DefaultWidth = (ushort)value;
+				UpdateOptions();
+			};
+
+			_height.ValueChanged += (double value) =>
+			{
+				_yat.Options.DefaultHeight = (ushort)value;
+				UpdateOptions();
+			};
+
+			_autoScroll.Toggled += (bool pressed) =>
+			{
+				_yat.Options.AutoScroll = pressed;
+				UpdateOptions();
+			};
+
+			_showPrompt.Toggled += (bool pressed) =>
+			{
+				_yat.Options.ShowPrompt = pressed;
+				UpdateOptions();
+			};
+		}
+
+		private void UpdateOptions() => _yat.EmitSignal(nameof(_yat.OptionsChanged), _yat.Options);
 	}
 
-	private void ConnectSignals()
-	{
-		CloseRequested += () => QueueFree();
-
-		_prompt.TextSubmitted += (string text) =>
-		{
-			_yat.Options.Prompt = text;
-			UpdateOptions();
-		};
-
-		_movable.Toggled += (bool pressed) =>
-		{
-			_yat.Options.WindowMovable = pressed;
-			UpdateOptions();
-		};
-
-		_width.ValueChanged += (double value) =>
-		{
-			_yat.Options.DefaultWidth = (ushort)value;
-			UpdateOptions();
-		};
-
-		_height.ValueChanged += (double value) =>
-		{
-			_yat.Options.DefaultHeight = (ushort)value;
-			UpdateOptions();
-		};
-
-		_autoScroll.Toggled += (bool pressed) =>
-		{
-			_yat.Options.AutoScroll = pressed;
-			UpdateOptions();
-		};
-
-		_showPrompt.Toggled += (bool pressed) =>
-		{
-			_yat.Options.ShowPrompt = pressed;
-			UpdateOptions();
-		};
-	}
-
-	private void UpdateOptions() => _yat.EmitSignal(nameof(_yat.OptionsChanged), _yat.Options);
 }
