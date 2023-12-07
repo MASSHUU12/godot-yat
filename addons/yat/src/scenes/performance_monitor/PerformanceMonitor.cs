da3cd41600f2f5ec28e0ff0e728268afaa38350a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-07 12:25:40 +0100

    Add support for adding components

--- addons/yat/src/scenes/performance_monitor/PerformanceMonitor.cs
+++ addons/yat/src/scenes/performance_monitor/PerformanceMonitor.cs
@@ -1,9 +1,38 @@
-using YAT.Scenes.YatWindow;
+using Godot;
+using YAT.Interfaces;
 
-public partial class PerformanceMonitor : YatWindow
+namespace YAT.Scenes.PerformanceMonitor
 {
-	public override void _Ready()
+	public partial class PerformanceMonitor : YatWindow.YatWindow
 	{
-		Move(WindowPosition.TopRight, 16);
+		private YAT _yat;
+		private Timer _timer;
+		private VBoxContainer _components;
+
+		public override void _Ready()
+		{
+			_yat = GetNode<YAT>("/root/YAT");
+			_timer = GetNode<Timer>("Timer");
+			_components = GetNode<VBoxContainer>("%Components");
+
+			Move(WindowPosition.TopRight, 16);
+
+			AddComponent(GD.Load<PackedScene>("res://addons/yat/src/scenes/performance_monitor/components/fps/Fps.tscn").Instantiate<Fps>());
+		}
+
+		public void AddComponent(Node component)
+		{
+			if (component is not IPerformanceMonitorComponent)
+			{
+				_yat.Terminal.Print(
+					$"Component {component.Name} does not implement IPerformanceMonitorComponent",
+					Terminal.Terminal.PrintType.Error
+				);
+				return;
+			}
+
+			_components.AddChild(component);
+			_timer.Timeout += (component as IPerformanceMonitorComponent).Update;
+		}
 	}
 }
