030a1c52b2d1b7c7c570ff2c9c5d79a98bf381ae
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-08 16:14:10 +0100

    Display process & physics time

--- addons/yat/src/scenes/monitor/components/fps/Fps.cs
+++ addons/yat/src/scenes/monitor/components/fps/Fps.cs
@@ -8,23 +8,38 @@ namespace YAT.Scenes.Monitor
 		public bool UseColors { get; set; }
 
 		private RichTextLabel _label;
+		private RichTextLabel _times;
 		private YAT _yat;
 
 		public override void _Ready()
 		{
 			_yat = GetNode<YAT>("/root/YAT");
-			_label = GetNode<RichTextLabel>("RichTextLabel");
+			_label = GetNode<RichTextLabel>("./VBoxContainer/RichTextLabel");
+			_times = GetNode<RichTextLabel>("./VBoxContainer/Times");
 		}
 
 		public void Update()
 		{
 			var fps = Performance.GetMonitor(Performance.Monitor.TimeFps);
+			var process = Performance.GetMonitor(Performance.Monitor.TimeProcess) * 1000;
+			var physics = Performance.GetMonitor(Performance.Monitor.TimePhysicsProcess) * 1000;
 			var opts = _yat.Options;
 
 			_label.Clear();
 			if (UseColors) _label.PushColor(fps < 30 ? opts.ErrorColor : opts.SuccessColor);
 			_label.AppendText($"{fps} FPS");
 			if (UseColors) _label.Pop();
+
+			_times.Clear();
+			_times.AppendText("Process: ");
+			if (UseColors && fps < 30) _times.PushColor(opts.ErrorColor);
+			_times.AppendText($"{process:0.00} ms\n");
+			if (UseColors && fps < 30) _times.Pop();
+			_times.AppendText("Physics: ");
+			if (UseColors && fps < 30) _times.PushColor(opts.ErrorColor);
+			_times.AppendText($"{physics:0.000} ms");
+			if (UseColors && fps < 30) _times.Pop();
+
 		}
 	}
 }
