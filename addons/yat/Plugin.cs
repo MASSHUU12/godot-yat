fdf69ab58e102013f3c28c268f17a331be7f2614
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-12 13:22:41 +0100

    Bump version to 1.14.1-beta

--- addons/yat/Plugin.cs
+++ addons/yat/Plugin.cs
@@ -7,14 +7,14 @@ public partial class Plugin : EditorPlugin
 	public override void _EnterTree()
 	{
 		AddAutoloadSingleton("YAT", "res://addons/yat/src/YAT.tscn");
-		GD.Print("YAT loaded!");
+		GD.Print($"YAT {GetPluginVersion()} loaded!");
 		GD.PrintRich("Up to date information about YAT can be found at [url=https://github.com/MASSHUU12/godot-yat/tree/main]godot-yat[/url] on GitHub.");
 	}
 
 	public override void _ExitTree()
 	{
 		RemoveAutoloadSingleton("YAT");
-		GD.Print("YAT unloaded!");
+		GD.Print($"YAT {GetPluginVersion()} unloaded!");
 	}
 }
 #endif
