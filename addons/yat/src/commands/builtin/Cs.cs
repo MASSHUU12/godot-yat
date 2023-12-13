470a66e5bf4ac65fa2a45e256e9973beea7d61bc
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-13 13:32:06 +0100

    Add Cs command for scene change

---
+++ addons/yat/src/commands/builtin/Cs.cs
@@ -0,0 +1,68 @@
+using Godot;
+using YAT.Attributes;
+using YAT.Enums;
+using YAT.Interfaces;
+using YAT.Scenes.Terminal;
+
+namespace YAT.Commands
+{
+	[Command("cs", "Changes the scene.", "[b]Usage[/b]: cs [i]scene[/i]")]
+	[Argument("scene", "string", "The scene to change to.")]
+	public partial class Cs : Node, ICommand
+	{
+		[Signal]
+		public delegate void SceneAboutToChangeEventHandler(string scenePath);
+		[Signal]
+		public delegate void SceneChangedEventHandler(string scenePath);
+		[Signal]
+		public delegate void SceneChangeFailedEventHandler(string scenePath, FailureReason reason);
+
+		public enum FailureReason
+		{
+			SceneCantOpen,
+			SceneDoesNotExist,
+			SceneCantInstantiate,
+		}
+
+		public YAT Yat { get; set; }
+
+		public Cs(YAT Yat) => this.Yat = Yat;
+
+		public CommandResult Execute(params string[] args)
+		{
+			var scene = args[1];
+
+			if (!ResourceLoader.Exists(scene, typeof(PackedScene).Name))
+			{
+				EmitSignal(SignalName.SceneChangeFailed, scene, (short)FailureReason.SceneDoesNotExist);
+
+				Yat.Terminal.Print($"Scene '{scene}' does not exist.", Terminal.PrintType.Error);
+
+				return CommandResult.Failure;
+			}
+
+			EmitSignal(SignalName.SceneAboutToChange, scene);
+
+			var error = Yat.GetTree().ChangeSceneToFile(scene);
+
+			if (error != Error.Ok)
+			{
+				EmitSignal(SignalName.SceneChangeFailed, scene, (short)(
+					error == Error.CantOpen
+					? FailureReason.SceneCantOpen
+					: FailureReason.SceneCantInstantiate
+				));
+
+				Yat.Terminal.Print($"Failed to change scene to '{scene}'.", Terminal.PrintType.Error);
+
+				return CommandResult.Failure;
+			}
+
+			EmitSignal(SignalName.SceneChanged, scene);
+
+			Yat.Terminal.Print($"Changed scene to '{scene}'.", Terminal.PrintType.Success);
+
+			return CommandResult.Success;
+		}
+	}
+}
