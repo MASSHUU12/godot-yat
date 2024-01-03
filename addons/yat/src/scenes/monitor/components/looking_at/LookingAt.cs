441aed3a8c32a1bb97629cfe77b4dee2406b1845
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-03 16:36:41 +0100

    Add World helper class with RayCast method moved from LookingAt class

--- addons/yat/src/scenes/monitor/components/looking_at/LookingAt.cs
+++ addons/yat/src/scenes/monitor/components/looking_at/LookingAt.cs
@@ -1,5 +1,5 @@
 using Godot;
-using Godot.Collections;
+using YAT.Helpers;
 using YAT.Interfaces;
 
 namespace YAT.Scenes.Monitor
@@ -21,9 +21,13 @@ namespace YAT.Scenes.Monitor
 		public void Update()
 		{
 			var prefix = "Looking at: ";
-			var result = RayCast();
+			var result = World.RayCast(_yat.GetViewport(), RAY_LENGTH);
 
-			if (result is null) return;
+			if (result is null)
+			{
+				_label.Text = prefix + "No camera";
+				return;
+			}
 
 			if (result.Count == 0)
 			{
@@ -41,28 +45,6 @@ namespace YAT.Scenes.Monitor
 			}
 
 			_label.Text = prefix + node.Name + " at " + position;
-		}
-
-		private Dictionary RayCast()
-		{
-			var viewport = _yat.GetViewport();
-			var camera = viewport.GetCamera3D();
-			var mousePos = viewport.GetMousePosition();
-			var prefix = "Looking at: ";
-
-			if (camera == null)
-			{
-				_label.Text = prefix + "No camera";
-				return null;
-			}
-
-			var origin = camera.ProjectRayOrigin(mousePos);
-			var end = origin + camera.ProjectRayNormal(mousePos) * RAY_LENGTH;
-			var query = PhysicsRayQueryParameters3D.Create(origin, end);
-			query.CollideWithAreas = true;
-			query.CollideWithBodies = true;
-
-			return camera.GetWorld3D().DirectSpaceState.IntersectRay(query);
 		}
 	}
 }
