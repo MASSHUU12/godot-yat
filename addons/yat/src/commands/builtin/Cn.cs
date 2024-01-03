47c213e565ceb8448f1d1d932d2fa7abaf7caf98
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-03 18:14:55 +0100

    Add option to switch node to the one the camera is looking at

--- addons/yat/src/commands/builtin/Cn.cs
+++ addons/yat/src/commands/builtin/Cn.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using Godot;
 using YAT.Attributes;
 using YAT.Enums;
+using YAT.Helpers;
 using YAT.Interfaces;
 using YAT.Scenes.Terminal;
 
@@ -18,14 +20,49 @@ namespace YAT.Commands
 
 		public Cn(YAT Yat) => this.Yat = Yat;
 
+		private const float DEFAULT_RAY_LENGTH = 256;
+		private const char RAY_CAST_PREFIX = '&';
+
 		public CommandResult Execute(Dictionary<string, object> cArgs, params string[] args)
 		{
 			var path = cArgs["node_path"] as string;
-			var result = Yat.Terminal.SelectedNode.ChangeSelectedNode(path);
+			bool result;
+
+			// If the path starts with RAY_CAST_PREFIX use RayCast to get the node path
+			// where the camera is looking at
+			if (path.StartsWith(RAY_CAST_PREFIX)) result = ChangeSelectedNode(GetNodePath(path));
+			else result = ChangeSelectedNode(path);
 
 			if (!result) Yat.Terminal.Print($"Invalid node path: {path}", Terminal.PrintType.Error);
 
 			return result ? CommandResult.Success : CommandResult.Failure;
+		}
+
+		private NodePath GetNodePath(string path)
+		{
+			var result = World.RayCast(Yat.GetViewport(), GetRayLength(path));
+
+			if (result is null)
+			{
+				Yat.Terminal.Print("No collider found.", Terminal.PrintType.Error);
+				return null;
+			}
+
+			Node collider = result.TryGetValue("collider", out Variant value) ? value.As<Node>() : null;
+
+			return collider?.GetPath();
+		}
+
+		private static float GetRayLength(string path)
+		{
+			return NumericHelper.TryConvert(path[1..], out float rayLength)
+				? rayLength
+				: DEFAULT_RAY_LENGTH;
+		}
+
+		private bool ChangeSelectedNode(NodePath path)
+		{
+			return Yat.Terminal.SelectedNode.ChangeSelectedNode(path);
 		}
 	}
 }
