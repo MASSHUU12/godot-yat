3a853a19593969f5d769959fecdc901a473035cd
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-06 19:26:41 +0100

    Comment-out _Input method

--- addons/yat/src/scenes/editor_terminal/EditorTerminal.cs
+++ addons/yat/src/scenes/editor_terminal/EditorTerminal.cs
@@ -2,10 +2,22 @@ using Godot;
 
 namespace YAT.Scenes.EditorTerminal
 {
+	[Tool]
 	public partial class EditorTerminal : Control
 	{
+		public BaseTerminal.BaseTerminal BaseTerminal { get; private set; }
+
 		public override void _Ready()
 		{
+			BaseTerminal = GetNode<BaseTerminal.BaseTerminal>("Content/BaseTerminal");
 		}
+
+		// public override void _Input(InputEvent @event)
+		// {
+		// 	if (@event.IsActionPressed("yat_toggle"))
+		// 	{
+		// 		CallDeferred("emit_signal", nameof(BaseTerminal.CloseRequested));
+		// 	}
+		// }
 	}
 }
