b5168bdcedea355edfa4cc544da063ceafbd8769
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-06 18:11:34 +0100

    Remove EditorTerminal when plugin is unloaded

--- addons/yat/Plugin.cs
+++ addons/yat/Plugin.cs
@@ -1,11 +1,14 @@
 #if TOOLS
 using Godot;
+using YAT.Scenes.EditorTerminal;
 
 [Tool]
 public partial class Plugin : EditorPlugin
 {
 	private string _version;
 	private const string _name = "YAT";
+
+	private Control _editorTerminal;
 
 	public override void _Notification(int what)
 	{
@@ -14,19 +17,22 @@ public partial class Plugin : EditorPlugin
 
 	public override void _EnterTree()
 	{
+		_editorTerminal = GD.Load<PackedScene>("res://addons/yat/src/scenes/editor_terminal/EditorTerminal.tscn").Instantiate<Control>();
+
 		AddAutoloadSingleton(_name, "res://addons/yat/src/YAT.tscn");
 		AddControlToBottomPanel(
-			GD.Load<PackedScene>("res://addons/yat/src/scenes/editor_terminal/EditorTerminal.tscn").Instantiate<Control>(),
+			_editorTerminal,
 			"Terminal"
 		);
 
 		GD.Print($"{_name} {_version} loaded!");
-		GD.PrintRich("Up to date information about YAT can be found at [url=https://github.com/MASSHUU12/godot-yat/tree/main]godot-yat[/url] on GitHub.");
+		GD.PrintRich("Up to date information about YAT can be found at [url=https://github.com/MASSHUU12/godot-yat/tree/main]https://github.com/MASSHUU12/godot-yat/tree/main[/url].");
 	}
 
 	public override void _ExitTree()
 	{
 		RemoveAutoloadSingleton(_name);
+		RemoveControlFromBottomPanel(_editorTerminal);
 		GD.Print($"{_name} {_version} unloaded!");
 	}
 }
