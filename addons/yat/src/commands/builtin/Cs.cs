bb1c2e70ce0ebac9d07562c9984076d5e8591f9b
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-07 23:01:32 +0100

    Rework cs command

--- addons/yat/src/commands/builtin/Cs.cs
+++ addons/yat/src/commands/builtin/Cs.cs
@@ -24,26 +24,22 @@ namespace YAT.Commands
 			SceneCantInstantiate,
 		}
 
-		public YAT Yat { get; set; }
-
-		public Cs(YAT Yat) => this.Yat = Yat;
-
-		public CommandResult Execute(params string[] args)
+		public CommandResult Execute(CommandArguments args)
 		{
-			var scene = args[1];
+			var scene = args.Arguments[1];
 
 			if (!ResourceLoader.Exists(scene, typeof(PackedScene).Name))
 			{
 				EmitSignal(SignalName.SceneChangeFailed, scene, (short)FailureReason.SceneDoesNotExist);
 
-				Yat.Terminal.Print($"Scene '{scene}' does not exist.", PrintType.Error);
+				args.Terminal.Print($"Scene '{scene}' does not exist.", PrintType.Error);
 
 				return CommandResult.Failure;
 			}
 
 			EmitSignal(SignalName.SceneAboutToChange, scene);
 
-			var error = Yat.GetTree().ChangeSceneToFile(scene);
+			var error = args.Yat.GetTree().ChangeSceneToFile(scene);
 
 			if (error != Error.Ok)
 			{
@@ -53,14 +49,14 @@ namespace YAT.Commands
 					: FailureReason.SceneCantInstantiate
 				));
 
-				Yat.Terminal.Print($"Failed to change scene to '{scene}'.", PrintType.Error);
+				args.Terminal.Print($"Failed to change scene to '{scene}'.", PrintType.Error);
 
 				return CommandResult.Failure;
 			}
 
 			EmitSignal(SignalName.SceneChanged, scene);
 
-			Yat.Terminal.Print($"Changed scene to '{scene}'.", PrintType.Success);
+			args.Terminal.Print($"Changed scene to '{scene}'.", PrintType.Success);
 
 			return CommandResult.Success;
 		}
