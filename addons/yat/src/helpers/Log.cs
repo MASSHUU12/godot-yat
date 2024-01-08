63107043980eb773e83c2a54850c3927d8b308e5
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-08 22:28:46 +0100

    Implement log methods

--- addons/yat/src/helpers/Log.cs
+++ addons/yat/src/helpers/Log.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.Text;
 using Godot;
 using YAT.Scenes.BaseTerminal;
 using static YAT.Scenes.BaseTerminal.BaseTerminal;
@@ -6,52 +9,137 @@ namespace YAT.Helpers
 {
 	public sealed class Log
 	{
+		public static BaseTerminal Terminal { get; set; }
+
+		[Flags]
 		public enum LogOutput
 		{
-			Terminal,
-			Editor,
-			EditorRich
+			None = 0b000,
+			Terminal = 0b001,
+			Editor = 0b010,
+			EditorRich = 0b100
 		}
 
-		private static LogOutput[] _enabledOutputs = new[] { LogOutput.Terminal };
+		private static LogOutput _enabledOutputs = LogOutput.Terminal;
 
-		public Log(params LogOutput[] enabledOutputs)
+		public Log(LogOutput enabledOutputs)
 		{
 			_enabledOutputs = enabledOutputs;
 		}
 
 		public static void Print(string message)
 		{
-			foreach (LogOutput output in _enabledOutputs)
-			{
-				switch (output)
-				{
-					case LogOutput.Terminal:
-						GD.Print(message);
-						break;
-					case LogOutput.Editor:
-						GD.Print(message);
-						break;
-					case LogOutput.EditorRich:
-						GD.PrintRich(message);
-						break;
-				}
-			}
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.Print(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PrintRich(message);
 		}
 
 		public static void Error(string message)
-		{ }
+		{
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message, PrintType.Error);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.PushError(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PushError(message);
+		}
 
 		public static void Warning(string message)
-		{ }
+		{
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message, PrintType.Warning);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.PushWarning(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PushWarning(message);
+		}
 
 		public static void Info(string message)
-		{ }
+		{
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message, PrintType.Normal);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.Print(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PrintRich(message);
+		}
+
+		public static void Success(string message)
+		{
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message, PrintType.Success);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.Print(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PrintRich(message);
+		}
 
 		public static void Debug(string message)
-		{ }
+		{
+#if DEBUG
+			message = $"[DEBUG] {message}";
 
-		public static void Trace(string message)
-		{ }
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.Print(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PrintRich(message);
+#endif
+		}
+
+		public static void Trace(string message, bool detailed = false)
+		{
+#if DEBUG
+			if (!detailed) message = $"[TRACE] {message}: {System.Environment.StackTrace}";
+			else
+			{
+				StackTrace st = new();
+				StringBuilder sb = new();
+
+				sb.Append($"[TRACE] {message}");
+
+				for (int i = 0; i < st.FrameCount; i++)
+				{
+					StackFrame sf = st.GetFrame(i);
+
+					sb.Append(string.Format(
+						"\n{0}({1},{2}): {3}.{4}",
+						sf.GetFileName(),
+						sf.GetFileLineNumber(),
+						sf.GetFileColumnNumber(),
+						sf.GetMethod().DeclaringType,
+						sf.GetMethod())
+					);
+				}
+
+				message = sb.ToString();
+			}
+
+			if ((_enabledOutputs & LogOutput.Terminal) == LogOutput.Terminal)
+				Terminal.Print(message);
+
+			if ((_enabledOutputs & LogOutput.Editor) == LogOutput.Editor)
+				GD.Print(message);
+
+			if ((_enabledOutputs & LogOutput.EditorRich) == LogOutput.EditorRich)
+				GD.PrintRich(message);
+#endif
+		}
 	}
 }
