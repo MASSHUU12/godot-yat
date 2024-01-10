7f353e70a75d28bf33b874dd41ccca5fa5b42c04
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-10 15:14:29 +0100

    Add InputInfo component

---
+++ addons/yat/src/scenes/base_terminal/components/input_info/InputInfo.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+namespace YAT.Scenes.BaseTerminal.Components.InputInfo
+{
+	public partial class InputInfo : PanelContainer
+	{
+		public RichTextLabel _text;
+		private MarginContainer _container;
+
+		public override void _Ready()
+		{
+			_text = GetNode<RichTextLabel>("%Text");
+			_container = GetNode<MarginContainer>("./MarginContainer");
+			Visible = false;
+		}
+
+		public void DisplayCommandInfo(string text)
+		{
+			_text.Text = text;
+			Visible = true;
+		}
+	}
+}
