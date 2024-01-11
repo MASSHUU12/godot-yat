599a05553fefceb7ee4541c2bee9c22dfe487ab0
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-11 11:58:38 +0100

    Refactor Cn command to support node tree search

--- addons/yat/src/commands/builtin/Cn.cs
+++ addons/yat/src/commands/builtin/Cn.cs
@@ -17,14 +17,19 @@ namespace YAT.Commands
 		"node_path",
 		"string",
 		"The node path of the new selected node.\n" +
-		"Use [i]&[/i] to use the RayCast method to get the node path where the camera is looking at.\n" +
-		"Use [i]&[b]ray_length[/b][/i] to specify the ray length (default 256).\n" +
-		"Example: [i]&[/i] or [i]&[b]256[/b][/i]"
+		"Use [b]&[/b] to use the RayCast method to get the node path where the camera is looking at.\n" +
+		"Use [b]&[/b][i]ray_length[/i] to specify the ray length (default 256).\n" +
+		"Example: [b]&[/b] or [b]&[/b][i]256[/i]" +
+		"\nUse [b]?[/b] to search the node path in the whole tree.\n" +
+		"Use [b]??[/b] to search the node path in the current node children.\n"
 	)]
 	public partial class Cn : ICommand
 	{
-		private const float DEFAULT_RAY_LENGTH = 256;
 		private const char RAY_CAST_PREFIX = '&';
+		private const float DEFAULT_RAY_LENGTH = 256;
+
+		private const string TREE_DEEP_SEARCH_PREFIX = "?";
+		private const string TREE_SHALLOW_SEARCH_PREFIX = "??";
 
 		private YAT _yat;
 		private BaseTerminal _terminal;
@@ -37,17 +42,39 @@ namespace YAT.Commands
 			_yat = args.Yat;
 			_terminal = args.Terminal;
 
-			// If the path starts with RAY_CAST_PREFIX use RayCast to get the node path
-			// where the camera is looking at
-			if (path.StartsWith(RAY_CAST_PREFIX)) result = ChangeSelectedNode(GetNodePath(path));
+			if (Text.StartsWith(path, TREE_SHALLOW_SEARCH_PREFIX, TREE_DEEP_SEARCH_PREFIX))
+				result = ChangeSelectedNode(GetNodeFromSearch(path));
+			else if (path.StartsWith(RAY_CAST_PREFIX)) result = ChangeSelectedNode(GetRayCastedNodePath(path));
 			else result = ChangeSelectedNode(path);
 
-			if (!result) args.Terminal.Print($"Invalid node path: {path}", PrintType.Error);
+			if (!result)
+			{
+				args.Terminal.Print($"Invalid node path: {path}", PrintType.Error);
+				return CommandResult.Failure;
+			}
 
-			return result ? CommandResult.Success : CommandResult.Failure;
+			return CommandResult.Success;
 		}
 
-		private NodePath GetNodePath(string path)
+		private NodePath GetNodeFromSearch(string path)
+		{
+			Node result;
+
+			if (path.StartsWith(TREE_SHALLOW_SEARCH_PREFIX))
+				result = World.SearchNode(_terminal.SelectedNode.Current, path[2..]);
+			else
+				result = World.SearchNode(_yat.GetTree().Root, path[1..]);
+
+			if (result is null)
+			{
+				_terminal.Print("No node found.", PrintType.Error);
+				return null;
+			}
+
+			return result.GetPath();
+		}
+
+		private NodePath GetRayCastedNodePath(string path)
 		{
 			var result = World.RayCast(_yat.GetViewport(), GetRayLength(path));
 
