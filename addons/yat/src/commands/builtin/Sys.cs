3698b9ac695ca85df6f099806bdf69837af14b6d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-16 18:21:50 +0100

    Add threaded attribute to Sys command

--- addons/yat/src/commands/builtin/Sys.cs
+++ addons/yat/src/commands/builtin/Sys.cs
@@ -2,7 +2,6 @@ using YAT.Attributes;
 using YAT.Enums;
 using YAT.Helpers;
 using YAT.Interfaces;
-using YAT.Scenes.BaseTerminal;
 
 namespace YAT.Commands
 {
@@ -17,18 +16,15 @@ namespace YAT.Commands
 		"The command to run (if contains more than one word, you need to wrap it in the parentheses)."
 	)]
 	[Option("-program", "string", "The program to run the command with (default to systems specific terminal).", "")]
+	[Threaded]
 	public sealed class Sys : ICommand
 	{
-		private BaseTerminal _terminal;
-
 		public CommandResult Execute(CommandArguments args)
 		{
 			var program = (string)args.ConvertedArgs["-program"];
 			var command = (string)args.ConvertedArgs["command"];
 			var commandName = command.Split(' ')[0];
 			var commandArgs = command[commandName.Length..].Trim() ?? string.Empty;
-
-			_terminal = args.Terminal;
 
 			OS.RunCommand(commandName, program, commandArgs);
 
