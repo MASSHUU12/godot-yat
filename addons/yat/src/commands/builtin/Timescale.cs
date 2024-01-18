39c6f1f66b844a0fba205c9906cd84ed481db793
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-18 16:26:20 +0100

    Add Timescale command

---
+++ addons/yat/src/commands/builtin/Timescale.cs
@@ -0,0 +1,25 @@
+using Godot;
+using YAT.Attributes;
+using YAT.Enums;
+using YAT.Interfaces;
+
+namespace YAT.Commands
+{
+	[Command(
+		"timescale",
+		"Sets the timescale.",
+		"[b]Usage[/b]: timescale"
+	)]
+	[Option("-set", "float", "Sets the timescale to the specified number.", 1.0f)]
+	public sealed class Timescale : ICommand
+	{
+		public CommandResult Execute(CommandData data)
+		{
+			var scale = (float)data.Options["-set"];
+
+			Engine.TimeScale = scale;
+
+			return CommandResult.Success;
+		}
+	}
+}
