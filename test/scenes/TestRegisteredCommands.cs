4d422aff6ec2003528b4006c6d05024c2ae59b9f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-25 19:52:56 +0100

    Add TestRegisteredCommands.cs with test cases for adding commands

---
+++ test/scenes/TestRegisteredCommands.cs
@@ -0,0 +1,50 @@
+using System;
+using YAT.Attributes;
+using YAT.Enums;
+using YAT.Interfaces;
+using YAT.Scenes;
+
+namespace GdUnit4
+{
+	using static Assertions;
+	using static YAT.Scenes.RegisteredCommands;
+
+	[TestSuite]
+	public partial class TestRegisteredCommands
+	{
+		[Command("test")]
+		private class TestCommand : ICommand
+		{
+			public CommandResult Execute(CommandData data)
+			{
+				return CommandResult.NotImplemented;
+			}
+		}
+
+		private class TestCommandWithoutAttribute : ICommand
+		{
+			public CommandResult Execute(CommandData data)
+			{
+				return CommandResult.NotImplemented;
+			}
+		}
+
+		[Command("test")]
+		private class TestCommandWithoutInterface
+		{
+			public CommandResult Execute(CommandData data)
+			{
+				return CommandResult.NotImplemented;
+			}
+		}
+
+		[TestCase(typeof(TestCommand), AddingResult.Success)]
+		[TestCase(typeof(TestCommand), AddingResult.ExistentCommand)]
+		[TestCase(typeof(TestCommandWithoutAttribute), AddingResult.MissingAttribute)]
+		[TestCase(typeof(TestCommandWithoutInterface), AddingResult.UnknownCommand)]
+		public void TestAddCommand(Type commandType, AddingResult expected)
+		{
+			AssertObject(AddCommand(commandType)).IsEqual(expected);
+		}
+	}
+}
