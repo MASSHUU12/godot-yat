fb5277d2addb901b8b2bad2cfd2c41d8ece18bcb
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-25 16:54:46 +0100

    Update builtin commands registration

--- addons/yat/src/YAT.cs
+++ addons/yat/src/YAT.cs
@@ -2,9 +2,8 @@ using System;
 using Godot;
 using YAT.Commands;
 using YAT.Helpers;
-using YAT.Interfaces;
+using YAT.Systems;
 using YAT.Scenes.BaseTerminal;
-using YAT.Scenes.CommandManager;
 using YAT.Scenes.OptionsManager;
 using YAT.Scenes.TerminalManager;
 
@@ -18,8 +17,8 @@ namespace YAT
 		public Node Windows { get; private set; }
 		public BaseTerminal CurrentTerminal { get; set; }
 
+		public RegisteredCommands Commands { get; private set; }
 		public OptionsManager OptionsManager { get; private set; }
-		public CommandManager CommandManager { get; private set; }
 		public TerminalManager TerminalManager { get; private set; }
 
 		public override void _Ready()
@@ -52,36 +51,41 @@ namespace YAT
 
 			Windows = GetNode<Node>("./Windows");
 
-			CommandManager = GetNode<CommandManager>("./CommandManager");
-			CommandManager.AddCommand(new Type[] {
-				typeof(Ls),
-				typeof(Ip),
-				typeof(Cn),
-				typeof(Cs),
-				typeof(Cls),
-				typeof(Man),
-				typeof(Set),
-				typeof(Cat),
-				typeof(Sys),
-				typeof(Quit),
-				typeof(Echo),
-				typeof(List),
-				typeof(View),
-				typeof(Ping),
-				typeof(Wenv),
-				typeof(Pause),
-				typeof(Watch),
-				typeof(Stats),
-				typeof(Reset),
-				typeof(Cowsay),
-				// typeof(Options),
-				typeof(Restart),
-				typeof(History),
-				typeof(Whereami),
-				typeof(Timescale),
-				typeof(ToggleAudio),
-				typeof(Commands.QuickCommands)
-			});
+			Commands = GetNode<RegisteredCommands>("./RegisteredCommands");
+			Commands.Ready += () =>
+			{
+				Commands.AddCommand(
+					new[] {
+					typeof(Ls),
+					typeof(Ip),
+					typeof(Cn),
+					typeof(Cs),
+					typeof(Cls),
+					typeof(Man),
+					typeof(Set),
+					typeof(Cat),
+					typeof(Sys),
+					typeof(Quit),
+					typeof(Echo),
+					typeof(List),
+					typeof(View),
+					typeof(Ping),
+					typeof(Wenv),
+					typeof(Pause),
+					typeof(Watch),
+					typeof(Stats),
+					typeof(Reset),
+					typeof(Cowsay),
+					// typeof(Options),
+					typeof(Restart),
+					typeof(History),
+					typeof(Whereami),
+					typeof(Timescale),
+					typeof(ToggleAudio),
+					typeof(Commands.QuickCommands)
+					}
+				);
+			};
 
 			Keybindings.LoadDefaultActions();
 		}
