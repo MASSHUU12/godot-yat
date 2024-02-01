063288c9d135b481b60bf29216131abd76161d29
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-01 18:52:27 +0100

    Update template for extensible command

--- script_templates/Node/ExtensibleCommand.cs
+++ script_templates/Node/ExtensibleCommand.cs
@@ -1,6 +1,7 @@
 // meta-description: Base template for extensible YAT commands.
 
 using _BINDINGS_NAMESPACE_;
+using System;
 using YAT.Attributes;
 using YAT.Enums;
 using YAT.Interfaces;
@@ -8,19 +9,17 @@ using YAT.Types;
 using static YAT.Scenes.BaseTerminal.BaseTerminal;
 
 [Command("_CLASS_", "Lorem ipsum dolor sit amet.", "[b]Usage[/b]: _CLASS_ [i]action[/i]")]
+[Argument("action", "string", "The name of the action to run.")]
 public partial class _CLASS_ : Extensible, ICommand
 {
 	public CommandResult Execute(CommandData data)
 	{
-		var option = data.Arguments[1];
+		var extensions = GetCommandExtensions("_CLASS_");
 
-		if (Extensions.ContainsKey(option))
-		{
-			var extension = Extensions[option];
-			return extension.Execute(data with { Arguments = data.Arguments[1..] });
-		}
+		if (extensions.TryGetValue((string)data.Arguments["action"], out Type extension))
+			return ExecuteExtension(extension, data with { RawData = data.RawData[1..] });
 
-		data.Terminal.Print("Options not found.", PrintType.Error);
+		data.Terminal.Print("Action not found.", PrintType.Error);
 		return CommandResult.Failure;
 	}
 }
