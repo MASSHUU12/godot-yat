2f20c2bcfd3213e64df88769472b85446655a406
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-01 10:34:01 +0100

    Simplify namespace declaration for attributes

--- addons/yat/src/attributes/ArgumentAttribute.cs
+++ addons/yat/src/attributes/ArgumentAttribute.cs
@@ -1,40 +1,39 @@
 using System;
 using System.Linq;
 
-namespace YAT.Attributes
+namespace YAT.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public sealed class ArgumentAttribute : Attribute
 {
-	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-	public sealed class ArgumentAttribute : Attribute
+	public string Name { get; private set; }
+	public object Type { get; set; }
+	public string Description { get; private set; }
+
+	public ArgumentAttribute(string name, string type, string Description = "")
 	{
-		public string Name { get; private set; }
-		public object Type { get; set; }
-		public string Description { get; private set; }
+		Name = name;
+		Type = ParseDataType(type);
+		this.Description = Description;
+	}
 
-		public ArgumentAttribute(string name, string type, string Description = "")
+	/// <summary>
+	/// Parses a string representation of a data type and returns the corresponding object or type.
+	/// </summary>
+	/// <param name="dataType">The string representation of the data type to parse.</param>
+	/// <returns>The parsed object or type, or null if the data type could not be parsed.</returns>
+	private static object ParseDataType(string dataType)
+	{
+		var data = dataType?.Trim();
+
+		if (string.IsNullOrEmpty(data)) return null;
+
+		if (data.StartsWith("[") && data.EndsWith("]"))
 		{
-			Name = name;
-			Type = ParseDataType(type);
-			this.Description = Description;
+			string[] values = data.Trim('[', ']').Split(',').Select(v => v.Trim()).ToArray();
+			return values;
 		}
 
-		/// <summary>
-		/// Parses a string representation of a data type and returns the corresponding object or type.
-		/// </summary>
-		/// <param name="dataType">The string representation of the data type to parse.</param>
-		/// <returns>The parsed object or type, or null if the data type could not be parsed.</returns>
-		private static object ParseDataType(string dataType)
-		{
-			var data = dataType?.Trim();
-
-			if (string.IsNullOrEmpty(data)) return null;
-
-			if (data.StartsWith("[") && data.EndsWith("]"))
-			{
-				string[] values = data.Trim('[', ']').Split(',').Select(v => v.Trim()).ToArray();
-				return values;
-			}
-
-			return data;
-		}
+		return data;
 	}
 }
