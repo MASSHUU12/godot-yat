d27e47786c5355f93860906f775e300803b6b4db
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-04 20:40:19 +0100

    Create ParseMethod method

--- addons/yat/src/classes/Parser.cs
+++ addons/yat/src/classes/Parser.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using YAT.Helpers;
 
 namespace YAT.Classes;
@@ -6,8 +8,27 @@ public static class Parser
 {
 	public static string[] ParseCommand(string command)
 	{
-		string[] input = Text.SanitizeText(command);
+		string[] tokens = Text.SanitizeText(command);
 
-		return Text.ConcatenateSentence(input);
+		return Text.ConcatenateSentence(tokens);
+	}
+
+	public static Tuple<string, string[]> ParseMethod(string method)
+	{
+		string[] tokens = Text.SanitizeText(method);
+		List<string> args = new();
+
+		if (tokens.Length == 0) return new(string.Empty, Array.Empty<string>());
+
+		var parts = (tokens.Length <= 1 ? tokens[^1] : tokens[0]).Split('(', 2,
+			StringSplitOptions.RemoveEmptyEntries |
+			StringSplitOptions.TrimEntries
+		);
+
+		if (parts.Length > 1) args.Add(parts[1][..^1]);
+
+		if (tokens.Length >= 1) for (int i = 1; i < tokens.Length; i++) args.Add(tokens[i][..^1]);
+
+		return new(parts[0], args.ToArray());
 	}
 }
