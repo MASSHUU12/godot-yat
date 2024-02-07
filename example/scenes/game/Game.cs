a28869eb8d0b2aaac3b93ac7ede4526215b5bce2
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-07 20:53:10 +0100

    Update game scene

---
+++ example/scenes/game/Game.cs
@@ -0,0 +1,44 @@
+using Godot;
+using YAT.Classes;
+using YAT.Helpers;
+
+public partial class Game : Node3D
+{
+	private Label3D _hint;
+
+	public override void _Ready()
+	{
+		_hint = GetNode<Label3D>("%Hint");
+
+		RegisterCommand();
+		SetHint();
+	}
+
+	private void SetHint()
+	{
+		var inputMap = InputMap.GetActions();
+
+		foreach (var action in inputMap)
+		{
+			if (action == Keybindings.TerminalToggle)
+			{
+				var inputEvent = InputMap.ActionGetEvents(action);
+
+				_hint.Text = inputEvent.Count > 0
+							? $"Press {inputEvent[0].AsText()} to open the YAT overlay."
+							: "Please set a key to open the YAT overlay.";
+				return;
+			}
+		}
+
+		_hint.Text = "Please create yat_toggle input map.";
+	}
+
+	private void RegisterCommand()
+	{
+		var cube = GetNode<MeshInstance3D>("Scene/Cube");
+
+		SetCube.Cube = cube;
+		Extensible.RegisterExtension("set", typeof(SetCube));
+	}
+}
