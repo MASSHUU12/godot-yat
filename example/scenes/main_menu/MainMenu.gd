100b793d1659bc9d26885aa9da5cf86d34686ed6
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-07 17:39:43 +0100

    Move MainMenu & Player to the scenes folder

--- example/scenes/main_menu/MainMenu.gd
+++ example/scenes/main_menu/MainMenu.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://example/game/Game.tscn")
+	get_tree().change_scene_to_file("res://example/scenes/game/Game.tscn")
 
 
 func _on_quit_pressed() -> void:
