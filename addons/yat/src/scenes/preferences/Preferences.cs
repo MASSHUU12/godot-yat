d4434df9b4420820e185079f9af99967dcfbe64f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-11 13:32:03 +0100

    Fix attached script & export YatPreferences

--- addons/yat/src/scenes/preferences/Preferences.cs
+++ addons/yat/src/scenes/preferences/Preferences.cs
@@ -1,5 +1,9 @@
+using Godot;
+using YAT.Resources;
+
 namespace YAT.Scenes;
 
 public partial class Preferences : YatWindow.YatWindow
 {
+	[Export] YatPreferences YatPreferences { get; set; } = new();
 }
