86cd481d8d68ec2eaf1dae5837f93253acae9f0c
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-13 13:15:11 +0100

    Update Monitor namespace

--- addons/yat/src/commands/builtin/Stats.cs
+++ addons/yat/src/commands/builtin/Stats.cs
@@ -5,7 +5,7 @@ using YAT.Attributes;
 using YAT.Enums;
 using YAT.Interfaces;
 using YAT.Scenes.BaseTerminal;
-using YAT.Scenes.Monitor;
+using YAT.Scenes;
 using YAT.Types;
 
 namespace YAT.Commands;
