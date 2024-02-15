6ff8116f261f3c9237c1d19c41e90b6cc90f268f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-15 14:27:17 +0100

    Create Networking class

---
+++ addons/yat/src/helpers/Networking.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using YAT.Types;
+
+namespace YAT.Helpers;
+
+public static class Networking
+{
+	/// <summary>
+	/// Returns a list of IP addresses that a packet would take to reach the specified host. <br />
+	/// Inspired by https://stackoverflow.com/questions/142614/traceroute-and-ping-in-c-sharp/45565253#45565253
+	/// </summary>
+	public static IEnumerable<IPAddress> GetTraceRoute(string hostname, NetworkingOptions options = null)
+	{
+		options ??= new();
+
+		byte[] buffer = CreateBuffer(options.BufferSize);
+
+		using var ping = new Ping();
+
+		for (ushort ttl = 1; ttl <= options.TTL; ttl++)
+		{
+			PingOptions pOptions = new(ttl, true);
+			PingReply reply = ping.Send(hostname, options.Timeout, buffer, pOptions);
+
+			// Route has been found
+			if (reply.Status == IPStatus.Success || reply.Status == IPStatus.TtlExpired)
+				yield return reply.Address;
+
+			// Route has not been found or the host is unreachable
+			if (reply.Status != IPStatus.TtlExpired && reply.Status != IPStatus.TimedOut)
+				break;
+		}
+	}
+
+	public static byte[] CreateBuffer(uint size)
+	{
+		byte[] buffer = new byte[size];
+		new Random().NextBytes(buffer);
+		return buffer;
+	}
+}
