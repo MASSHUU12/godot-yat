8bfa72ee324b07fcb334fb12408daac5dfd37246
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-15 14:13:47 +0100

    Add NetworkingOptions record

---
+++ addons/yat/src/types/NetworkingOptions.cs
@@ -0,0 +1,9 @@
+namespace YAT.Types;
+
+public record NetworkingOptions
+{
+	public ushort Timeout { get; init; } = 10000;
+	public ushort TTL { get; init; } = 30;
+	public ushort BufferSize { get; init; } = 32;
+	public bool DontFragment { get; init; } = true;
+}
