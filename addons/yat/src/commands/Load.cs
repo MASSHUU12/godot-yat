dc5c28510fc912af9df5f9451906136a60acbd43
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-17 13:26:43 +0100

    Add support for 2D

--- addons/yat/src/commands/Load.cs
+++ addons/yat/src/commands/Load.cs
@@ -28,7 +28,7 @@ public sealed class Load : ICommand
 
 		if (!ValidatePath(path)) return ICommand.Failure("Invalid object path.");
 
-		return LoadNode3D(path, data);
+		return !is2D ? LoadNode3D(path, data) : LoadNode2D(path, data);
 	}
 
 	private static CommandResult LoadNode3D(string path, CommandData data)
@@ -52,14 +52,15 @@ public sealed class Load : ICommand
 		);
 		var hidden = (bool)data.Options["-hidden"];
 
-		if (camera is null) return ICommand.Failure("No 3D camera found.");
+		if (camera is null && !absolute)
+			return ICommand.Failure("No 3D camera found.");
 
 		if (GD.Load<PackedScene>(path).Instantiate() is not Node3D node)
 			return ICommand.Failure("Failed to load object.");
 
 		data.Yat.GetTree().Root.AddChild(node);
 
-		TransformNode(node, camera, position, rotation, scale, hidden, absolute);
+		TransformNode3D(node, camera, position, rotation, scale, hidden, absolute);
 
 		return ICommand.Success(
 			string.Format(
@@ -72,7 +73,43 @@ public sealed class Load : ICommand
 		);
 	}
 
-	private static void TransformNode(
+	private static CommandResult LoadNode2D(string path, CommandData data)
+	{
+		var camera = data.Yat.GetViewport().GetCamera2D();
+		var absolute = (bool)data.Options["-absolute"];
+		var position = new Vector2(
+			(float)data.Options["-x"],
+			(float)data.Options["-y"]
+		);
+		var rotation = (float)data.Options["-rz"];
+		var scale = new Vector2(
+			(float)data.Options["-sx"],
+			(float)data.Options["-sy"]
+		);
+		var hidden = (bool)data.Options["-hidden"];
+
+		if (camera is null && !absolute)
+			return ICommand.Failure("No 2D camera found.");
+
+		if (GD.Load<PackedScene>(path).Instantiate() is not Node2D node)
+			return ICommand.Failure("Failed to load object.");
+
+		data.Yat.GetTree().Root.AddChild(node);
+
+		TransformNode2D(node, camera, position, rotation, scale, hidden, absolute);
+
+		return ICommand.Success(
+			string.Format(
+				"Object '{0}' loaded at {1} with rotation {2} and scale {3}.",
+				node.Name,
+				node.Position,
+				node.Rotation,
+				node.Scale
+			)
+		);
+	}
+
+	private static void TransformNode3D(
 		Node3D node,
 		Camera3D camera,
 		Vector3 position,
@@ -85,6 +122,26 @@ public sealed class Load : ICommand
 		if (absolute) node.GlobalPosition = position;
 		else node.GlobalPosition = camera.GlobalTransform.Origin
 								+ camera.GlobalTransform.Basis * position;
+
+		node.GlobalRotation = rotation;
+		node.Scale = scale;
+		node.Visible = !hidden;
+	}
+
+	private static void TransformNode2D(
+		Node2D node,
+		Camera2D camera,
+		Vector2 position,
+		float rotation,
+		Vector2 scale,
+		bool hidden,
+		bool absolute
+	)
+	{
+		if (absolute) node.GlobalPosition = position;
+		else node.GlobalPosition = camera.GlobalTransform.Origin
+								+ camera.GlobalTransform.X * position.X
+								+ camera.GlobalTransform.Y * position.Y;
 
 		node.GlobalRotation = rotation;
 		node.Scale = scale;
