0f2b92d22c790d0827a3b271361cb74133409381
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-19 11:25:27 +0100

    Overloaded IsWithinRange method to support multiple types

--- addons/yat/src/helpers/Numeric.cs
+++ addons/yat/src/helpers/Numeric.cs
@@ -5,15 +5,13 @@ namespace YAT.Helpers;
 
 public static class Numeric
 {
-	/// <summary>
-	/// Determines whether a value is within the specified range (inclusive).
-	/// </summary>
-	/// <typeparam name="T">The type of the values to compare.</typeparam>
-	/// <param name="value">The value to check.</param>
-	/// <param name="min">The minimum value of the range.</param>
-	/// <param name="max">The maximum value of the range.</param>
-	/// <returns>True if the value is within the range, false otherwise.</returns>
 	public static bool IsWithinRange<T>(T value, T min, T max) where T : IComparable<T>
+	{
+		return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+	}
+
+	public static bool IsWithinRange<T, E>(T value, E min, E max)
+	where T : IComparable<T>, IComparable<E>
 	{
 		return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
 	}
