87bcb8ee73b96ce86f1329bfdd02c937a67f28ff
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-25 16:00:02 +0100

    Create RunSelected method

--- addons/yat/src/scenes/debug_screen/DebugScreen.cs
+++ addons/yat/src/scenes/debug_screen/DebugScreen.cs
@@ -17,9 +17,13 @@ public partial class DebugScreen : Control
 		_bottomLeftContainer,
 		_bottomRightContainer;
 
-	public static readonly List<IDebugScreenItem>[] debugScreens =
+	public static readonly List<IDebugScreenItem>[] registeredItems =
 	// Use EDebugScreenItemPosition enum values as indices
-	new List<IDebugScreenItem>[4] { new(), new(), new(), new() };
+	new List<IDebugScreenItem>[4] {
+		new()
+		{
+			GD.Load<PackedScene>("uid://0e2nft11f3h1").Instantiate<FpsItem>()
+		}, new(), new(), new() };
 
 	public override void _Ready()
 	{
@@ -28,14 +32,68 @@ public partial class DebugScreen : Control
 		_bottomLeftContainer = GetNode<VBoxContainer>("%BottomLeftContainer");
 		_bottomRightContainer = GetNode<VBoxContainer>("%BottomRightContainer");
 
-		RemoveChildren(_topLeftContainer);
-		RemoveChildren(_topRightContainer);
-		RemoveChildren(_bottomLeftContainer);
-		RemoveChildren(_bottomRightContainer);
+		RemoveAllChildren();
 
 		_timer = GetNode<Timer>("Timer");
 		_timer.WaitTime = UpdateInterval;
 		_timer.Timeout += OnTimerTimeout;
+
+		RunSelected("FPS");
+	}
+
+	public void RunSelected(params string[] titles)
+	{
+		RemoveAllChildren();
+
+		foreach (string title in titles)
+		{
+			var lowerTitle = title.ToLower();
+
+			AddItemToContainer(
+				registeredItems[(int)EDebugScreenItemPosition.TopLeft],
+				_topLeftContainer,
+				lowerTitle
+			);
+			AddItemToContainer(
+				registeredItems[(int)EDebugScreenItemPosition.TopRight],
+				_topRightContainer,
+				lowerTitle
+			);
+			AddItemToContainer(
+				registeredItems[(int)EDebugScreenItemPosition.BottomLeft],
+				_bottomLeftContainer,
+				lowerTitle
+			);
+			AddItemToContainer(
+				registeredItems[(int)EDebugScreenItemPosition.BottomRight],
+				_bottomRightContainer,
+				lowerTitle
+			);
+		}
+	}
+
+	private static void AddItemToContainer(
+		List<IDebugScreenItem> items,
+		VBoxContainer container,
+		string lowerTitle
+	)
+	{
+		foreach (IDebugScreenItem item in items)
+		{
+			if (item.Title.ToLower() == lowerTitle)
+			{
+				container.AddChild((item as Node).Duplicate());
+				break;
+			}
+		}
+	}
+
+	private void RemoveAllChildren()
+	{
+		RemoveChildren(_topLeftContainer);
+		RemoveChildren(_topRightContainer);
+		RemoveChildren(_bottomLeftContainer);
+		RemoveChildren(_bottomRightContainer);
 	}
 
 	private void OnTimerTimeout()
@@ -46,7 +104,7 @@ public partial class DebugScreen : Control
 		children.AddRange(_bottomLeftContainer.GetChildren());
 		children.AddRange(_bottomRightContainer.GetChildren());
 
-		foreach (Node child in children) if (child is IDebugScreenItem item) item.Update();
+		foreach (Node child in children) (child as IDebugScreenItem)?.Update();
 	}
 
 	private static void RemoveChildren(VBoxContainer container)
@@ -62,18 +120,18 @@ public partial class DebugScreen : Control
 	{
 		if (item is not IDebugScreenItem debugItem) return false;
 
-		if (debugScreens[(int)position].Contains(debugItem)) return false;
+		if (registeredItems[(int)position].Contains(debugItem)) return false;
 
-		debugScreens[(int)position].Add(debugItem);
+		registeredItems[(int)position].Add(debugItem);
 
 		return true;
 	}
 
 	public static bool UnregisterItem(IDebugScreenItem item, EDebugScreenItemPosition position)
 	{
-		if (!debugScreens[(int)position].Contains(item)) return false;
+		if (!registeredItems[(int)position].Contains(item)) return false;
 
-		debugScreens[(int)position].Remove(item);
+		registeredItems[(int)position].Remove(item);
 
 		return true;
 	}
