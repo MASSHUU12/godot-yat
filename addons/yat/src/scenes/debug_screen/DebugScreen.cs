fef35e2b38bc7d01d4b91d397e5cfcd260c3b56a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-25 13:47:16 +0100

    Setup basic grid

--- addons/yat/src/scenes/debug_screen/DebugScreen.cs
+++ addons/yat/src/scenes/debug_screen/DebugScreen.cs
@@ -1,9 +1,10 @@
+using Godot;
+
 namespace YAT.Scenes;
 
-public partial class DebugScreen : YatWindow
+public partial class DebugScreen : CanvasLayer
 {
 	public override void _Ready()
 	{
-		base._Ready();
 	}
 }
