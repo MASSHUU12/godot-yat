49cef4a457724e08a5732a7307eb410e9d5c6d72
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-25 15:00:56 +0100

    Add item registration, timer, etc.

--- addons/yat/src/interfaces/IDebugScreenItem.cs
+++ addons/yat/src/interfaces/IDebugScreenItem.cs
@@ -2,5 +2,7 @@ namespace YAT.Interfaces;
 
 public interface IDebugScreenItem
 {
+	public string Title { get; set; }
+
 	public void Update();
 }
--- addons/yat/src/scenes/debug_screen/DebugScreen.cs
+++ addons/yat/src/scenes/debug_screen/DebugScreen.cs
@@ -1,14 +1,25 @@
+using System.Collections.Generic;
 using Godot;
+using YAT.Enums;
+using YAT.Interfaces;
 
 namespace YAT.Scenes;
 
-public partial class DebugScreen : CanvasLayer
+public partial class DebugScreen : Control
 {
+	[Export(PropertyHint.Range, "0.05, 5, 0.1")]
+	public float UpdateInterval { get; set; } = 0.5f;
+
+	private Timer _timer;
 	private VBoxContainer
 		_topLeftContainer,
 		_topRightContainer,
 		_bottomLeftContainer,
 		_bottomRightContainer;
+
+	public static readonly List<IDebugScreenItem>[] debugScreens =
+	// Use EDebugScreenItemPosition enum values as indices
+	new List<IDebugScreenItem>[4] { new(), new(), new(), new() };
 
 	public override void _Ready()
 	{
@@ -21,6 +32,21 @@ public partial class DebugScreen : CanvasLayer
 		RemoveChildren(_topRightContainer);
 		RemoveChildren(_bottomLeftContainer);
 		RemoveChildren(_bottomRightContainer);
+
+		_timer = GetNode<Timer>("Timer");
+		_timer.WaitTime = UpdateInterval;
+		_timer.Timeout += OnTimerTimeout;
+	}
+
+	private void OnTimerTimeout()
+	{
+		List<Node> children = new();
+		children.AddRange(_topLeftContainer.GetChildren());
+		children.AddRange(_topRightContainer.GetChildren());
+		children.AddRange(_bottomLeftContainer.GetChildren());
+		children.AddRange(_bottomRightContainer.GetChildren());
+
+		foreach (Node child in children) if (child is IDebugScreenItem item) item.Update();
 	}
 
 	private static void RemoveChildren(VBoxContainer container)
@@ -30,5 +56,25 @@ public partial class DebugScreen : CanvasLayer
 			container.RemoveChild(child);
 			child.QueueFree();
 		}
+	}
+
+	public static bool RegisterItem(Node item, EDebugScreenItemPosition position)
+	{
+		if (item is not IDebugScreenItem debugItem) return false;
+
+		if (debugScreens[(int)position].Contains(debugItem)) return false;
+
+		debugScreens[(int)position].Add(debugItem);
+
+		return true;
+	}
+
+	public static bool UnregisterItem(IDebugScreenItem item, EDebugScreenItemPosition position)
+	{
+		if (!debugScreens[(int)position].Contains(item)) return false;
+
+		debugScreens[(int)position].Remove(item);
+
+		return true;
 	}
 }
