3a74371ae447f85fed4b304f59ccf29ca5125e5b
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-02-25 13:56:26 +0100

    Cleanup containers on _Ready

--- addons/yat/src/scenes/debug_screen/DebugScreen.cs
+++ addons/yat/src/scenes/debug_screen/DebugScreen.cs
@@ -4,7 +4,31 @@ namespace YAT.Scenes;
 
 public partial class DebugScreen : CanvasLayer
 {
+	private VBoxContainer
+		_topLeftContainer,
+		_topRightContainer,
+		_bottomLeftContainer,
+		_bottomRightContainer;
+
 	public override void _Ready()
 	{
+		_topLeftContainer = GetNode<VBoxContainer>("%TopLeftContainer");
+		_topRightContainer = GetNode<VBoxContainer>("%TopRightContainer");
+		_bottomLeftContainer = GetNode<VBoxContainer>("%BottomLeftContainer");
+		_bottomRightContainer = GetNode<VBoxContainer>("%BottomRightContainer");
+
+		RemoveChildren(_topLeftContainer);
+		RemoveChildren(_topRightContainer);
+		RemoveChildren(_bottomLeftContainer);
+		RemoveChildren(_bottomRightContainer);
+	}
+
+	private static void RemoveChildren(VBoxContainer container)
+	{
+		foreach (Node child in container.GetChildren())
+		{
+			container.RemoveChild(child);
+			child.QueueFree();
+		}
 	}
 }
