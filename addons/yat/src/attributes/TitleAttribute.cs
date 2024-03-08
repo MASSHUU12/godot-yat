fc62be5829896d61e3269a62e28f9b04e633f4d3
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-08 11:21:09 +0100

    Add Title attribute

---
+++ addons/yat/src/attributes/TitleAttribute.cs
@@ -0,0 +1,12 @@
+namespace YAT.Attributes;
+
+[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false)]
+public sealed class TitleAttribute : System.Attribute
+{
+	public string Title { get; private set; }
+
+	public TitleAttribute(string title)
+	{
+		Title = title;
+	}
+}
