2aad997da05d2bcd1b03e5f1a35812c058fbd2d8
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-13 12:30:44 +0100

    Refactor Whereami command

--- addons/yat/src/commands/Whereami.cs
+++ addons/yat/src/commands/Whereami.cs
@@ -18,30 +18,25 @@ public sealed class Whereami : ICommand
 		var e = (bool)data.Options["-e"];
 		var longForm = (bool)data.Options["-l"];
 
-		if (e)
-		{
-			PrintExecutablePath(data.Terminal);
-			return ICommand.Success();
-		}
-
-		PrintSceneInfo(data.Terminal, s, longForm);
-		return ICommand.Success();
+		return ICommand.Ok(
+			e
+			? GetExecutablePath(data.Terminal)
+			: GetSceneInfo(data.Terminal, s, longForm)
+		);
 	}
 
-	private static void PrintExecutablePath(BaseTerminal terminal) =>
-		terminal.Print(OS.GetExecutablePath().GetBaseDir());
+	private static string GetExecutablePath(BaseTerminal terminal) =>
+		OS.GetExecutablePath().GetBaseDir();
 
-	private static void PrintSceneInfo(BaseTerminal terminal, bool s, bool l)
+	private static string GetSceneInfo(BaseTerminal terminal, bool s, bool l)
 	{
 		var scene = terminal.GetTree().CurrentScene;
 		scene = s ? terminal.SelectedNode.Current : scene;
 
-		terminal.Print(
-			string.Format(
-				"{0} {1}",
-				scene.GetPath(),
-				l ? "(" + scene.SceneFilePath + ")" : string.Empty
-			)
+		return string.Format(
+			"{0} {1}",
+			scene.GetPath(),
+			l ? "(" + scene.SceneFilePath + ")" : string.Empty
 		);
 	}
 }
