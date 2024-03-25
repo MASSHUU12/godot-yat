f9d852100fb2cb528be7e4a404b16d75d93292fd
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-25 16:07:43 +0100

    Update OptionAttribute to allow null default values

--- addons/yat/src/attributes/OptionAttribute.cs
+++ addons/yat/src/attributes/OptionAttribute.cs
@@ -5,9 +5,9 @@ namespace YAT.Attributes;
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public sealed class OptionAttribute : CommandInputAttribute
 {
-	public object DefaultValue { get; private set; }
+	public object? DefaultValue { get; private set; }
 
-	public OptionAttribute(string name, string type, string description = "", object defaultValue = null)
+	public OptionAttribute(string name, string type, string description = "", object? defaultValue = null)
 	: base(name, type, description)
 	{
 		DefaultValue = defaultValue;
