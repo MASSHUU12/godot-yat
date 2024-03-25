215311ff686f51bd805b6269fc32ff1fffc546ad
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-25 16:25:35 +0100

    Add default constructor for CommandInputType

--- addons/yat/src/types/CommandInputType.cs
+++ addons/yat/src/types/CommandInputType.cs
@@ -2,4 +2,7 @@ using Godot;
 
 namespace YAT.Types;
 
-public record CommandInputType(StringName Type, float Min, float Max, bool IsArray);
+public record CommandInputType(StringName Type, float Min, float Max, bool IsArray)
+{
+	public CommandInputType() : this(string.Empty, 0.0f, 0.0f, false) { }
+}
