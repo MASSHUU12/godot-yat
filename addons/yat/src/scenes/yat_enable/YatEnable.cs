4fbeb151f04f97705fb263972d390070324a6b19
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-31 18:41:30 +0200

    feat: handle cmd argument

--- addons/yat/src/scenes/yat_enable/YatEnable.cs
+++ addons/yat/src/scenes/yat_enable/YatEnable.cs
@@ -1,5 +1,5 @@
+using System.Linq;
 using Godot;
-using YAT.Classes.Managers;
 
 namespace YAT.Scenes;
 
@@ -8,23 +8,36 @@ public partial class YatEnable : Node
 	[Export(PropertyHint.Flags, "UserData,CurrentDirectory,CmdArgument")]
 	public short YatEnableAction { get; set; } = 0;
 
-#nullable disable
-	[Export] public PreferencesManager PreferencesManager { get; set; }
-#nullable restore
+	[Export] public string FileName { get; set; } = ".yatenable";
+	[Export] public string ArgumentName { get; set; } = "--yat";
 
 	public bool YatEnabled { get; set; } = true;
 
 	public override void _Ready()
 	{
-		if (!PreferencesManager.Preferences.UseYatEnableFile) return;
+		if (YatEnableAction == 0) return;
 
-		var path = PreferencesManager.Preferences.YatEnableLocation switch
+		if ((YatEnableAction & 0b0001) == 1)
 		{
-			Enums.EYatEnableLocation.UserData => "user://",
-			Enums.EYatEnableLocation.CurrentDirectory => "res://",
-			_ => "user://"
-		};
+			YatEnabled = FileAccess.FileExists("user://" + FileName);
 
-		YatEnabled = FileAccess.FileExists(path + PreferencesManager.Preferences.YatEnableFile);
+			if (YatEnabled) return;
+		}
+
+		if ((YatEnableAction & 0b0010) == 2)
+		{
+			YatEnabled = FileAccess.FileExists("res://" + FileName);
+
+			if (YatEnabled) return;
+		}
+
+		if ((YatEnableAction & 0b0100) == 4)
+		{
+			YatEnabled = OS.GetCmdlineUserArgs().Contains(ArgumentName);
+
+			if (YatEnabled) return;
+		}
+
+		YatEnabled = false;
 	}
 }
