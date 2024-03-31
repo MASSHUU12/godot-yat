e18972ca22525c5d4dd64fea58acee8f7dd09d8f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-31 17:50:17 +0200

    chore: move YatEnable functionality from YAT.cs to YatEnable.cs

--- addons/yat/src/scenes/yat_enable/YatEnable.cs
+++ addons/yat/src/scenes/yat_enable/YatEnable.cs
@@ -1,4 +1,5 @@
 using Godot;
+using YAT.Classes.Managers;
 
 namespace YAT.Scenes;
 
@@ -7,7 +8,23 @@ public partial class YatEnable : Node
 	[Export(PropertyHint.Flags, "UserData,CurrentDirectory,CmdArgument")]
 	public short YatEnableAction { get; set; } = 0;
 
+#nullable disable
+	[Export] public PreferencesManager PreferencesManager { get; set; }
+#nullable restore
+
+	public bool YatEnabled { get; set; } = true;
+
 	public override void _Ready()
 	{
+		if (!PreferencesManager.Preferences.UseYatEnableFile) return;
+
+		var path = PreferencesManager.Preferences.YatEnableLocation switch
+		{
+			Enums.EYatEnableLocation.UserData => "user://",
+			Enums.EYatEnableLocation.CurrentDirectory => "res://",
+			_ => "user://"
+		};
+
+		YatEnabled = FileAccess.FileExists(path + PreferencesManager.Preferences.YatEnableFile);
 	}
 }
