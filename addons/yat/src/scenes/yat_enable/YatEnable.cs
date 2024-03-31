645fe9e6817824bb4be5b1b0b37c5845dfcbe1c6
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-31 17:41:55 +0200

    feat: Export YatEnableAction bitmask

--- addons/yat/src/scenes/yat_enable/YatEnable.cs
+++ addons/yat/src/scenes/yat_enable/YatEnable.cs
@@ -4,6 +4,9 @@ namespace YAT.Scenes;
 
 public partial class YatEnable : Node
 {
+	[Export(PropertyHint.Flags, "UserData,CurrentDirectory,CmdArgument")]
+	public short YatEnableAction { get; set; } = 0;
+
 	public override void _Ready()
 	{
 	}
