7476964aacd81d9ae06a0df96d6cb67ee59f7d66
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-06-24 19:24:47 +0200

    feat: Add default values and empty constructors to CommandType & CommandTypeRanged

--- addons/yat/src/classes/CommandType.cs
+++ addons/yat/src/classes/CommandType.cs
@@ -4,12 +4,14 @@ namespace YAT.Classes;
 
 public class CommandType
 {
-	public ECommandInputType Type { get; private set; }
-	public bool IsArray { get; private set; }
+	public ECommandInputType Type { get; private set; } = ECommandInputType.Void;
+	public bool IsArray { get; private set; } = false;
 
 	public CommandType(ECommandInputType type, bool isArray)
 	{
 		Type = type;
 		IsArray = isArray;
 	}
+
+	public CommandType() { }
 }
--- addons/yat/src/classes/CommandTypeRanged.cs
+++ addons/yat/src/classes/CommandTypeRanged.cs
@@ -4,12 +4,14 @@ namespace YAT.Classes;
 
 public class CommandTypeRanged : CommandType
 {
-	public float Min { get; private set; }
-	public float Max { get; private set; }
+	public float Min { get; private set; } = float.MinValue;
+	public float Max { get; private set; } = float.MaxValue;
 
 	public CommandTypeRanged(ECommandInputType type, bool isArray, float min, float max) : base(type, isArray)
 	{
 		Min = min;
 		Max = max;
 	}
+
+	public CommandTypeRanged() : base() { }
 }
