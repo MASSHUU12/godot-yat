0abb6164dd24b579fec0cf4256a6af5e35714b13
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-06-25 20:43:57 +0200

    feat: add GetTypeDefinition method to CommandType class

--- addons/yat/src/classes/CommandType.cs
+++ addons/yat/src/classes/CommandType.cs
@@ -4,9 +4,9 @@ namespace YAT.Classes;
 
 public class CommandType
 {
-	public string Name { get; private set; } = string.Empty;
-	public ECommandInputType Type { get; private set; } = ECommandInputType.Void;
-	public bool IsArray { get; private set; } = false;
+	public string Name { get; init; } = string.Empty;
+	public ECommandInputType Type { get; init; } = ECommandInputType.Void;
+	public bool IsArray { get; init; } = false;
 
 	public CommandType(string name, ECommandInputType type, bool isArray)
 	{
@@ -16,4 +16,9 @@ public class CommandType
 	}
 
 	public CommandType() { }
+
+	public virtual string GetTypeDefinition()
+	{
+		return Name;
+	}
 }
