fe60e6876af0e88a79b704697fe0e2e40f7885f9
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-07-06 18:40:23 +0200

    feat: store type definition in public variable

--- addons/yat/src/classes/CommandType.cs
+++ addons/yat/src/classes/CommandType.cs
@@ -4,21 +4,23 @@ namespace YAT.Classes;
 
 public class CommandType
 {
-	public string Name { get; init; } = string.Empty;
-	public ECommandInputType Type { get; init; } = ECommandInputType.Void;
-	public bool IsArray { get; init; } = false;
+    public string Name { get; init; } = string.Empty;
+    public ECommandInputType Type { get; init; } = ECommandInputType.Void;
+    public bool IsArray { get; init; } = false;
+    public string TypeDefinition { get; init; }
 
-	public CommandType(string name, ECommandInputType type, bool isArray)
-	{
-		Name = name;
-		Type = type;
-		IsArray = isArray;
-	}
+    public CommandType(string name, ECommandInputType type, bool isArray)
+    {
+        Name = name;
+        Type = type;
+        IsArray = isArray;
+        TypeDefinition = GenerateTypeDefinition();
+    }
 
-	public CommandType() { }
+    public CommandType()
+    {
+        TypeDefinition = GenerateTypeDefinition();
+    }
 
-	public virtual string GetTypeDefinition()
-	{
-		return Name;
-	}
+    protected virtual string GenerateTypeDefinition() => Name;
 }
