52620e3bffdbdc4f6f53e4ff25eb5d8b0991212a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-07-10 14:27:41 +0200

    feat: create HistoryComponent

---
+++ addons/yat/src/scenes/base_terminal/components/history_component/HistoryComponent.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Godot;
+
+namespace YAT.Scenes;
+
+public partial class HistoryComponent : Node
+{
+    public readonly LinkedList<string> History = new();
+    public LinkedListNode<string>? CurrentNode { get; private set; }
+
+    private YAT? _yat;
+
+    public override void _Ready()
+    {
+        _yat = GetNode<YAT>("/root/YAT");
+    }
+
+    public void Add(string command)
+    {
+        if (History.Count >= _yat!.PreferencesManager.Preferences.HistoryLimit)
+        {
+            return;
+        }
+
+        CurrentNode = null;
+        History.AddLast(command);
+    }
+
+    public LinkedListNode<string>? MovePrevious()
+    {
+        if (CurrentNode is null && History.Count > 0)
+        {
+            CurrentNode = History.Last;
+            return CurrentNode;
+        }
+        else if (CurrentNode?.Previous is not null)
+        {
+            CurrentNode = CurrentNode.Previous;
+            return CurrentNode;
+        }
+
+        return null;
+    }
+
+    public LinkedListNode<string>? MoveNext()
+    {
+        if (CurrentNode is not null && CurrentNode.Next is not null)
+        {
+            CurrentNode = CurrentNode.Next;
+            return CurrentNode;
+        }
+
+        CurrentNode = null;
+        return null;
+    }
+}
