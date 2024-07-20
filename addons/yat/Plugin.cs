e95e7cc9b972851797d26cbe1167ad5e2ac3e729
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-07-20 17:20:52 +0200

    feat: check for update on plugin start

--- addons/yat/Plugin.cs
+++ addons/yat/Plugin.cs
@@ -1,5 +1,8 @@
 #if TOOLS
 using Godot;
+using YAT.Classes;
+using YAT.Helpers;
+using YAT.Types;
 
 namespace YAT;
 
@@ -15,8 +18,10 @@ public partial class Plugin : EditorPlugin
         AddAutoloadSingleton("YAT", GetPluginPath() + "/src/YAT.tscn");
         AddAutoloadSingleton("DebugScreen", GetPluginPath() + "/src/scenes/debug_screen/DebugScreen.tscn");
 
-        GD.Print($"{_version} loaded!");
+        GD.Print($"YAT {_version} loaded!");
         GD.PrintRich("Up to date information about YAT can be found at [url=https://github.com/MASSHUU12/godot-yat/tree/main]https://github.com/MASSHUU12/godot-yat/tree/main[/url].");
+
+        UpdateCheck();
     }
 
     public override void _ExitTree()
@@ -30,6 +35,40 @@ public partial class Plugin : EditorPlugin
     private string GetPluginPath()
     {
         return GetScript().As<Script>().ResourcePath.GetBaseDir();
+    }
+
+    private static void UpdateCheck()
+    {
+        const bool DISABLE_UPDATE_CHECK = false;
+
+        if (DISABLE_UPDATE_CHECK)
+        {
+#pragma warning disable CS0162 // Unreachable code detected
+            return;
+#pragma warning restore CS0162 // Unreachable code detected
+        }
+
+        if (Updater.IsUpdateAvailable())
+        {
+            (bool isSuccess, ReleaseTagInfo? info) = Release.GetLatestVersion();
+
+            if (!isSuccess || info is null)
+            {
+                return;
+            }
+
+            GD.PrintRich(
+                string.Format(
+                    "New YAT version {0} is available, click [url={1}]here[/url] to download.",
+                    info.Version,
+                    info.ZipballUrl
+                )
+            );
+
+            return;
+        }
+
+        GD.Print("YAT is up-to-date.");
     }
 }
 #endif
