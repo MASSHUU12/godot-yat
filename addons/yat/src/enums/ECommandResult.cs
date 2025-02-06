01074dc74de7c76f97633663951b62987b35c4d3
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-02-06 18:13:45 +0100

    feat: create Pipeline class for commands chaining

--- addons/yat/src/enums/ECommandResult.cs
+++ addons/yat/src/enums/ECommandResult.cs
@@ -3,14 +3,14 @@ namespace YAT.Enums;
 public enum ECommandResult
 {
     Unavailable = -1,
-    Success,
-    Failure,
-    InvalidArguments,
-    InvalidCommand,
-    InvalidPermissions,
-    InvalidState,
-    NotImplemented,
-    UnknownCommand,
-    UnknownError,
-    Ok
+    Success = 0,
+    Failure = 1,
+    InvalidArguments = 2,
+    InvalidCommand = 3,
+    InvalidPermissions = 4,
+    InvalidState = 5,
+    NotImplemented = 6,
+    UnknownCommand = 7,
+    UnknownError = 8,
+    Ok = 9
 }
